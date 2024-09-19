4a142954ca5949c05ca5fad24b9679e94e711c20
Alan Gonzalez <74994720+AlanDev24@users.noreply.github.com>
2024-09-18 23:12:14 -0700

    Update server.ts
    Configurar puerto :p

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -2,32 +2,27 @@ import express, { Router } from "express";
 
 interface Options {
   routes: Router;
-  port: number
+  port: number;
 }
 
 export class Server {
   private app = express();
   private readonly routes: Router;
-  private readonly port: number
+  private readonly port: number;
 
   constructor(options: Options) {
     const { routes, port } = options;
     this.routes = routes;
-    this.port = port
+    this.port = port;
   }
 
   async start() {
-    const port = process.env.PORT;
-    
-    this.app.listen(port || 3000);
-
-    //* configuracion de JSON en peticiones
-    //* Puede agreagar mas middlewares para diferentes formatos
+    this.app.listen(this.port || 3000);
     this.app.use(express.json());
 
     //* Routes
     this.app.use(this.routes);
 
-    console.log(`App runing on port ${port}`);
+    console.log(`App runing on port ${this.port}`);
   }
 }
